4a357dea4fbdca60d35aafc9a5fac2089693bc60
joimar755 <joimarjose19@gmail.com>
2023-05-19 10:14:18 -0500

    React_up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import { Saludar } from './App';
+import { Menu } from './Menu';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
+    <Menu />
     <Saludar />
   </div>
 );
+
+
 
